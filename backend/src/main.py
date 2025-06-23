3f7d2c18a61c4dd9ebcc737831b330dc609a6e0e
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-06-23 10:23:02 +0200

    [Add] HTTP endpoints for customer costs

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,18 @@
 from fastapi  import FastAPI
 from pydantic import BaseModel
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from prometheus_client import Counter, Gauge
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from starlette.responses import Response
 
 
 log_counter = Counter("log_events_total", "Counts number of entries for every log level", ["level"])
+costs_total = Gauge("costs_total", "Summarizes total costs for different customers", ["customer"])
 
 class Message(BaseModel):
     message:str
+
+class Costs(BaseModel):
+    cost:str
 
 app = FastAPI()
 
@@ -35,6 +40,21 @@ async def log_endpoint_info(message:Message):
 async def log_endpoint_info(message:Message):
     log_counter.labels(level="CRITICAL").inc()
     return {"message": f"severity with log level 'CRITICAL' and content: {message}"} 
+
+@app.post("/cost/vodafone")
+async def cost_vodafone(cost: Costs):
+    costs_total.labels(customer="Vodafone").set(cost.cost)
+    return {"cost_vodafone": cost}
+    
+@app.post("/cost/telekom")
+async def cost_telekom(cost:Costs):
+    costs_total.labels(customer="Telekom").set(cost.cost)
+    return {"cost_telekom": cost}
+
+@app.post("/cost/1und1")
+async def cost_1und1(cost: Costs):
+    costs_total.labels(customer="1und1").set(cost.cost)
+    return {"cost_1und1": cost}
 
 @app.get("/metrics")
 async def metrics():
