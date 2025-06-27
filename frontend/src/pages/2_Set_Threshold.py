d65555d32d42f1ea4757afaee2eada7483b255bc
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-06-27 10:47:58 +0200

    [Change] put all cost thresholds into one form and add check if total cost threshold is exceeded

--- frontend/src/pages/2_Set_Threshold.py
+++ frontend/src/pages/2_Set_Threshold.py
@@ -1,33 +1,36 @@
 import streamlit as st 
 
-with st.form("Total cost threshold"):
+
+def check_if_cost_thresholds_less_or_equal_than_total_threshold(total_cost_threshold: float, vodafone_cost_threshold:float,
+    telekom_cost_threshold:float, _1und1_cost_threshold: float):
+    
+    customer_cost_threshold_sum = vodafone_cost_threshold + telekom_cost_threshold + _1und1_cost_threshold
+
+    if customer_cost_threshold_sum > total_cost_threshold:
+        return False
+    return True
+
+
+with st.form("Set thresholds"):
     st.write("Enter threshold to set total cost limit for all customers")
-    input_threshold_total_cost = st.number_input("Enter value")
-    submit_total_cost = st.form_submit_button("Submit")
+    input_threshold_total_cost = st.number_input("Enter value - total cost threshold")
 
-with st.form("Vodafone cost threshold"):
     st.write("Enter threshold to set the cost limit for customer Vodafone")
-    input_threshold_vodafone = st.number_input("Enter value")
-    submit_cost_threshold_vodafone = st.form_submit_button("Submit")
+    input_threshold_vodafone = st.number_input("Enter value - Vodafone cost threshold ")
 
-with st.form("Telekom cost threshold"):
     st.write("Enter threshold to set the cost limit for customer Telekom")
-    input_threshold_telekom = st.number_input("Enter value")
-    submit_cost_threshold_telekom = st.form_submit_button("Submit")
+    input_threshold_telekom = st.number_input("Enter value - Telekom cost threshold")
 
-with st.form("1und1 cost threshold"):
     st.write("Enter threshold to set the cost limit for customer 1und1")
-    input_threshold_1und1 = st.number_input("Enter value")
-    submit_cost_threshold_1und1 = st.form_submit_button("Submit")
+    input_threshold_1und1 = st.number_input("Enter value - 1und1 cost threshold")
 
-if submit_total_cost:
-    st.warning("HII")
+    submit_thresholds = st.form_submit_button("Submit")
 
-if submit_cost_threshold_vodafone:
-    pass
-
-if submit_cost_threshold_telekom:
-    pass
-
-if submit_cost_threshold_1und1:
-    pass
+if submit_thresholds:
+    customer_threshold_check = check_if_cost_thresholds_less_or_equal_than_total_threshold(input_threshold_total_cost, input_threshold_vodafone,
+        input_threshold_telekom, input_threshold_1und1)
+    
+    if not customer_threshold_check:
+        st.error("Please verify that the total cost threshold is not exceeded!")
+    else:
+        st.success("Thresholds have been set!")
