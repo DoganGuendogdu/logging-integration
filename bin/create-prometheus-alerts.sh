1cce2312fcfdfa5bcc2f8efcc6cfaae1ee759a65
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-07-03 11:30:12 +0200

    [Replace] contents of bash script with implementation using json file instead of .env

--- bin/create-prometheus-alerts.sh
+++ bin/create-prometheus-alerts.sh
@@ -1,27 +1,71 @@
 #!/bin/bash
 
-SCRIPT_PATH="$(readlink -f "$BASH_SOURCE")"      
-BIN_DIR="$(dirname "$SCRIPT_PATH")"               
+SCRIPT_PATH="$(readlink -f "$BASH_SOURCE")"
+BIN_DIR="$(dirname "$SCRIPT_PATH")"
 APP_HOME="$(dirname "$BIN_DIR")"
-
 PROMETHEUS_DIR="$APP_HOME"/prometheus
 
-echo "Creating alert rules (alert-rules.yml) from template file for Prometheus."
+echo "Creating alert rules (alert-rules.yml) for Prometheus."
 
-if [[ ! -e ${PROMETHEUS_DIR} ]]; then
-    echo "Failed to create aler rules."
-    echo "dir '${PROMETHEUS_DIR}' does not exist."
-    exit 1 
+echo "Reading configuration from 'config.json'."
+
+config_data=$(cat "$PROMETHEUS_DIR"/config.json)
+
+thresholds=$(echo "$config_data" | jq '.thresholds')
+thresholds_array=$(echo "$thresholds" | jq '.[]')
+total_cost=$(echo "$thresholds_array" | jq '.total_cost')
+vodafone_cost=$(echo "$thresholds_array" | jq '.vodafone_cost')
+telekom_cost=$(echo "$thresholds_array" | jq '.telekom_cost')
+cost_1und1=$(echo "$thresholds_array" | jq '."1und1_cost"')
+
+# if config file does not contain any data, raise error
+# -z: Checks if length of string is zero
+if [[ -z "$config_data" ]]; then
+  echo "Failed to retrieve config data. File is empty."
+  exit 1
 fi
 
-# grep -v '^#': grep contents from .env file while line does not match char '#' to ignore comments
-# | xargs: redirect env variables (as key-value pairs) to convert those into a single line argument list 
-# export: export env variables in shell by providing all vairables as a single line 
-export $(grep -v '^#' "$APP_HOME"/.env | xargs)
+echo "Retrieving configuration for thresholds."
 
-# envsubst: environment variable substitution by providing template files as input. Here, env var replaces by env var in shell
-# explicitily tell which env var to use to prevent unindentend overwriting of vars 
-# '>': redirect output to alert configuration file
+if [[ "$thresholds" == 'null' || -z "$thresholds" ]]; then
+  echo "Error: 'thresholds' is missing or null or empty."
+  exit 1
+fi
+
+if [[ "$thresholds_array" == 'null' || -z "$thresholds_array" ]]; then
+  echo "Error: 'thresholds' array is missing or null or empty."
+  exit 1
+fi
+
+if [[ "$total_cost" == "null" || -z "$total_cost" ]]; then
+  echo "Error: 'total_cost' is missing or null or empty."
+  exit 1
+fi
+
+if [[ "$vodafone_cost" == "null" || -z "$vodafone_cost" ]]; then
+  echo "Error: 'vodafone_cost' is missing or null or empty."
+  exit 1
+fi
+
+if [[ "$telekom_cost" == "null" || -z "$telekom_cost" ]]; then
+  echo "Error: 'telekom_cost' is missing or null or empty."
+  exit 1
+fi
+
+if [[ "$cost_1und1" == "null" || -z "$cost_1und1" ]]; then
+  echo "Error: '1und1_cost' is missing or null or empty."
+  exit 1
+fi
+
+export THRESHOLD_TOTAL_COST=$total_cost
+export THRESHOLD_COST_VODAFONE=$vodafone_cost
+export THRESHOLD_COST_TELEKOM=$telekom_cost
+export THRESHOLD_COST_1UND1=$cost_1und1
+
+echo "Successfully retrieved thresholds."
+
+echo "Creating alert-rules-template.yml"
+
 envsubst '${THRESHOLD_TOTAL_COST} ${THRESHOLD_COST_VODAFONE} ${THRESHOLD_COST_TELEKOM} ${THRESHOLD_COST_1UND1}'\
 < "$PROMETHEUS_DIR"/alert-rules-template.yml > "$PROMETHEUS_DIR"/alert-rules.yml
 
